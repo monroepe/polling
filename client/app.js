cc7b594f787f28b9f4e011b44b5c98a5fbe30cfa
Peter <monroepe@gmail.com>
2015-05-15 18:53:14 -0400

    Added ability to enter new poll

--- client/app.js
+++ client/app.js
@@ -5,3 +5,13 @@ Template.body.helpers({
   }
 
 });
+
+// adds index to each item
+UI.registerHelper('indexedArray', function(context, options) {
+  if (context) {
+    return context.map(function(item, index) {
+      item._index = index;
+      return item;
+    });
+  }
+});
